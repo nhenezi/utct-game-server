6a3fb0b656ef25e5972311bcd36bbc4597739519
Edita Kulović <edita.kulovic@hotmail.com>
2014-02-26 00:52:12 +0100

    function undo_move added, function make_move improved

--- algo/nextmoveminmax.py
+++ algo/nextmoveminmax.py
@@ -12,6 +12,7 @@ def send_move(main_board_move, boards_move, oldData):
   }
   rc.publish('nextMove', json.dumps(data))
   
+  
 def calculate_next_move(data):
   print data
   #...
@@ -27,14 +28,23 @@ def calculate_next_move(data):
     'boards': next_move_boards
   }
 
-def make_move(main_board_move, boards_move, data):
-#  print data
-  data['boards'][main_board_move][boards_move] = data['next_move']
-  data['main_board'][main_board_move] = utct.winner(data['boards'][main_board_move])
-  data['next_board'] = boards_move
-#  print data
-  return data
 
+def make_move(main_board_move, boards_move, data):
+  data['boards'][main_board_move][boards_move] = data['next_move']
+  data['main_board'][main_board_move] = utct.EMPTY_VALUE if utct.winner(data['boards'][main_board_move]) == False else utct.winner(data['boards'][main_board_move])
+  data['next_board'] = boards_move
+  data['next_move']= utct.PLAYER_X if data['next_move'] == utct.PLAYER_Y else utct.PLAYER_Y
+  return data
+  
+  
+def undo_move(main_board_move, boards_move, data):
+  data['boards'][main_board_move][boards_move] = utct.EMPTY_VALUE
+  data['main_board'][main_board_move] = utct.EMPTY_VALUE if utct.winner(data['boards'][main_board_move]) == False else utct.winner(data['boards'][main_board_move])
+  data['next_board'] = main_board_move
+  data['next_move'] = utct.PLAYER_X if data['next_move'] == utct.PLAYER_Y else utct.PLAYER_Y
+  return data
+  
+  
 if __name__ == "__main__":
   rc = redis.Redis();
   ps = rc.pubsub()
